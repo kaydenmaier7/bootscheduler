411af4d9a938a3667e837baee0f3a3404507c584
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-10-05 14:57:08 -0700

    Create migrations for boot and appointment

---
+++ db/migrate/20161005215010_create_appointments.rb
@@ -0,0 +1,15 @@
+class CreateAppointments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :appointments do |t|
+      t.time :time
+      t.date :date
+      t.string :location
+      t.references :mentor
+      t.references :topic
+      t.references :student
+
+
+      t.timestamps
+    end
+  end
+end
