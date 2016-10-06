d17284aeee4abf633a57b4cffc76ac68622dcb06
APilarca <angelpill15@gmail.com>
2016-10-05 17:36:40 -0700

    create other controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,38 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'feedbacks/new'
+
+  get 'feedbacks/create'
+
+  get 'feedbacks/edit'
+
+  get 'feedbacks/update'
+
+  get 'feedbacks/destroy'
+
+  get 'feedbacks/index'
+
+  get 'feedbacks/show'
+
+  get 'appointments/new'
+
+  get 'appointments/create'
+
+  get 'appointments/edit'
+
+  get 'appointments/update'
+
+  get 'appointments/destroy'
+
+  get 'appointments/index'
+
+  get 'appointments/show'
+
   get 'boots/new'
 
   get 'boots/create'
