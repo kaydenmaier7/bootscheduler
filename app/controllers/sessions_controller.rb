dd76cc3f5cd6a72d016bb4d8e2477db3175d6fb9
APilarca <angelpill15@gmail.com>
2016-10-05 19:11:31 -0700

    create routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	user = User.find_by(email: session_params['email'])
+  	if user && user.authenticate(session_params['password'])
+  		session[:user_id] = user.id
+  		redirect_to boot_path(user)
+  	else
+  		render 'new'
+  	end
   end
 
   def destroy
+  	session[:user_id] = nil
+  	render 'welcomes#index'
+  end
+  private
+  def session_params
+  	params.require(:session).permit(:email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,53 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcomes/index'
+  resources :boots do
+  	resources :feedbacks
+  	resources :appointments
+  end
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
-  get 'feedbacks/new'
-
-  get 'feedbacks/create'
-
-  get 'feedbacks/edit'
-
-  get 'feedbacks/update'
-
-  get 'feedbacks/destroy'
-
-  get 'feedbacks/index'
-
-  get 'feedbacks/show'
-
-  get 'appointments/new'
-
-  get 'appointments/create'
-
-  get 'appointments/edit'
-
-  get 'appointments/update'
-
-  get 'appointments/destroy'
-
-  get 'appointments/index'
-
-  get 'appointments/show'
-
-  get 'boots/new'
-
-  get 'boots/create'
-
-  get 'boots/edit'
-
-  get 'boots/udpate'
-
-  get 'boots/destroy'
-
-  get 'boots/index'
-
-  get 'boots/show'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:new, :create, :destroy]
+  root 'welcomes#index'
 end
