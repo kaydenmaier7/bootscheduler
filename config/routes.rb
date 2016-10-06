714215fbb412805c5246f3e483ed6194e55fd975
APilarca <angelpill15@gmail.com>
2016-10-05 17:31:36 -0700

    create boots controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'boots/new'
+
+  get 'boots/create'
+
+  get 'boots/edit'
+
+  get 'boots/udpate'
+
+  get 'boots/destroy'
+
+  get 'boots/index'
+
+  get 'boots/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
