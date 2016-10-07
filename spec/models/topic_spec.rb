d51f2a611e31d77293d5e47fec4415793e16faf3
rustyhutto <rhutto92@gmail.com>
2016-10-06 19:55:43 -0700

    Add tests for topic model

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "attributes" do
+    it { is_expected.to have_db_column(:name)}
+  end
+
+  describe "associations" do
+    it { is_expected.to have_and_belong_to_many(:boots) }
+    it { is_expected.to have_many(:appointments) }
+  end
 end
