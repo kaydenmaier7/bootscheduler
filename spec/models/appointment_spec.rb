75a82e85bf736278aec2be546ca313e4e250150c
rustyhutto <rhutto92@gmail.com>
2016-10-06 19:52:48 -0700

    Add tests for appointment model

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "attributes" do
+    it { is_expected.to have_db_column(:time)}
+    it { is_expected.to have_db_column(:date)}
+    it { is_expected.to have_db_column(:location)}
+  end
+
+  describe "associations" do
+    it { is_expected.to belong_to(:mentor).class_name("Boot") }
+    it { is_expected.to belong_to(:student).class_name("Boot") }
+    it { is_expected.to belong_to(:topic) }
+  end
 end
