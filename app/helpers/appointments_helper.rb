43c1a6bf5d7511051eb0fcb3da7d1d47da158884
Jessica Forchione & Louis Rowan <pair+jsforchione.louisrowan@devbootcamp.com>
2016-10-06 19:03:10 -0700

    fixes new appointment form and time display

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -1,2 +1,22 @@
 module AppointmentsHelper
+  def readable_time(time)
+    @time = time
+    @hour = @time.hour
+    @min = @time.min
+
+    if @min < 10
+      @min = "0" + @min.to_s
+    end
+
+    if @hour > 12
+      @hour = @hour - 12
+      @hour = @hour.to_s
+      @min = @min.to_s
+      @human_readable = "#{@hour}:#{@min} PM"
+    else
+      @hour = @hour.to_s
+      @min = @min.to_s
+      @human_readable = "#{@hour}:#{@min} AM"
+    end
+  end
 end
