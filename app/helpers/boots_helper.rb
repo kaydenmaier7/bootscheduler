d915daa45c7a6f11e4ad4eb6075087acf8106d99
Louis Rowan <email": null>
2016-10-07 16:37:58 -0700

    reformats date

--- app/helpers/boots_helper.rb
+++ app/helpers/boots_helper.rb
@@ -1,2 +1,10 @@
 module BootsHelper
+
+  def reformat_date(date)
+    date = date.to_s
+    date = date.split('-')
+    new_date = "#{date[1]}/#{date[2]}/#{date[0]}"
+    return new_date
+  end
+
 end
