0bfb835f278ae6443d5eab7ce1394613579a8ecc
rustyhutto <rhutto92@gmail.com>
2016-10-06 19:28:14 -0700

    Add tests for boot attributes

--- spec/models/boot_spec.rb
+++ spec/models/boot_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Boot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "attributes" do
+    it { is_expected.to have_db_column(:first_name) }
+    it { is_expected.to have_db_column(:last_name) }
+    it { is_expected.to have_db_column(:phone) }
+    it { is_expected.to have_db_column(:role) }
+    it { is_expected.to have_db_column(:email) }
+    it { is_expected.to have_db_column(:password_digest) }
+  end
+
+  describe "associations" do
+  end
 end
